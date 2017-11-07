61bacbffc59fa634de78d81fef6e20659469dfdf
alice <alice@corporation.net>
2017-11-07 10:01:01 -0500

    testing commit integration

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ var SampleApp = function() {
 
     /**
      *  Set up server IP address and port # using env variables/defaults.
+     *  test
      */
     self.setupVariables = function() {
         //  Set the environment variables we need.
